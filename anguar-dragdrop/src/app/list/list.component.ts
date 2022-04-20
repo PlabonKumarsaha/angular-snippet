0074515f4623177425daf9b7bdc0afcebcf0ad2e
pkumarsaha71@gmail.com <46686524+PlabonKumarsaha@users.noreply.github.com>
2022-04-21 00:59:03 +0600

    feat : category entity and dao created

--- anguar-dragdrop/src/app/list/list.component.ts
+++ anguar-dragdrop/src/app/list/list.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup,FormBuilder, Validators } from '@angular/forms';
+import {CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag-drop';
+
 
 @Component({
   selector: 'app-list',
@@ -13,6 +15,19 @@ export class ListComponent implements OnInit {
   inProgress : any[]= [];
   done : any[]= [];
 
+
+drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex,
+      );
+      }
+      }
 
   constructor(private formBuilder : FormBuilder) { }
 
