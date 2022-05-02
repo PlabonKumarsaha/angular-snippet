1264b572ce5946cce7306182407bde9966d1e427
pkumarsaha71@gmail.com <46686524+PlabonKumarsaha@users.noreply.github.com>
2022-05-03 04:00:42 +0600

    feat : dialog ref added

--- ReactiveFormsCrud/src/app/dialog/dialog.component.ts
+++ ReactiveFormsCrud/src/app/dialog/dialog.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-//import { MatDialogRef } from '@angular/material/dialog';
+import { MatDialogRef } from '@angular/material/dialog';
 import { ApiService } from '../services/api.service';
 
 @Component({
@@ -15,8 +15,7 @@ export class DialogComponent implements OnInit {
 
   constructor(private formBuilder : FormBuilder,
     private api : ApiService,
-   // private dialogRef : MatDialogRef<DialogComponent>
-   ) { }
+    private dialogRef : MatDialogRef<DialogComponent>) { }
 
   addProduct(){
     if(this.productForm.valid){
@@ -25,7 +24,7 @@ export class DialogComponent implements OnInit {
         next:(res)=>{
           alert('product added sucessfully')
           this.productForm.reset();
-          // this.dialogRef.close('save')
+          this.dialogRef.close('save')
         },
         error:()=>{
           alert('error occuered!')
