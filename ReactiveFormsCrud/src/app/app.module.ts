a9095c081b8344d072068deb6be844c07c7c3bf9
pkumarsaha71@gmail.com <46686524+PlabonKumarsaha@users.noreply.github.com>
2022-05-02 23:41:16 +0600

    feat : item catgeory mat select added

--- ReactiveFormsCrud/src/app/app.module.ts
+++ ReactiveFormsCrud/src/app/app.module.ts
@@ -11,6 +11,8 @@ import {MatDialogModule} from '@angular/material/dialog';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import { DialogComponent } from './dialog/dialog.component';
 import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
+
 
 
 @NgModule({
@@ -27,7 +29,8 @@ import {MatInputModule} from '@angular/material/input';
     MatButtonModule,
     MatDialogModule,
     MatFormFieldModule,
-    MatInputModule
+    MatInputModule,
+    MatSelectModule
   ],
   providers: [],
   bootstrap: [AppComponent]
