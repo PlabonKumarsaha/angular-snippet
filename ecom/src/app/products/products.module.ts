ccd0e39868e134eef0aad3ca5091303d5fe49e6d
pkumarsaha71@gmail.com <46686524+PlabonKumarsaha@users.noreply.github.com>
2022-05-07 00:26:55 +0600

    feat : lazy loading routing

--- ecom/src/app/products/products.module.ts
+++ ecom/src/app/products/products.module.ts
@@ -3,11 +3,21 @@ import { CommonModule } from '@angular/common';
 
 import { ProductsRoutingModule } from './products-routing.module';
 import { ProductsComponent } from './products.component';
+import { AddProductComponent } from './add-product/add-product.component';
+import { ViewProductComponent } from './view-product/view-product.component';
+import { UpdateProductComponent } from './update-product/update-product.component';
+import { ViewProductbyDateComponent } from './view-productby-date/view-productby-date.component';
+import { ViewProductByCategoryComponent } from './view-product-by-category/view-product-by-category.component';
 
 
 @NgModule({
   declarations: [
-    ProductsComponent
+    ProductsComponent,
+    AddProductComponent,
+    ViewProductComponent,
+    UpdateProductComponent,
+    ViewProductbyDateComponent,
+    ViewProductByCategoryComponent
   ],
   imports: [
     CommonModule,
