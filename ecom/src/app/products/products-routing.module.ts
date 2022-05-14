6d5aaa42848411fa4e04dbe6e790d1d301ebfe09
pkumarsaha71@gmail.com <46686524+PlabonKumarsaha@users.noreply.github.com>
2022-05-14 13:37:51 +0600

    wip : routes for and compionent deisgns set

--- ecom/src/app/products/products-routing.module.ts
+++ ecom/src/app/products/products-routing.module.ts
@@ -3,12 +3,17 @@ import { RouterModule, Routes } from '@angular/router';
 import { AddProductComponent } from './add-product/add-product.component';
 import { ProductsComponent } from './products.component';
 import { ViewAllProductComponent } from './view-all-product/view-all-product.component';
+import { ViewProductByCategoryComponent } from './view-product-by-category/view-product-by-category.component';
 import { ViewProductComponent } from './view-product/view-product.component';
+import { ViewProductbyDateComponent } from './view-productby-date/view-productby-date.component';
 
 const routes: Routes = [{ path: '', component: ProductsComponent },
           { path: 'add-product', component: AddProductComponent },
-          { path: 'view-product', component:ViewProductComponent },
-          { path: 'list-product', component: ViewAllProductComponent}
+          { path: 'view-product/:id', component:ViewProductComponent },
+          { path: 'list-product', component: ViewAllProductComponent},
+          { path: 'search-catgory', component: ViewProductByCategoryComponent },
+          { path: 'update-product/:id', component:ViewProductComponent },
+          { path: 'search-date', component: ViewProductbyDateComponent}
         ];
 
 @NgModule({
