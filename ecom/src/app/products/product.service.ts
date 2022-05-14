8149b1e96f5b9d959a56bf91cf412ab8e46d687f
pkumarsaha71@gmail.com <46686524+PlabonKumarsaha@users.noreply.github.com>
2022-05-14 23:45:10 +0600

    add : services added

--- ecom/src/app/products/product.service.ts
+++ ecom/src/app/products/product.service.ts
@@ -6,9 +6,30 @@ import { Injectable } from '@angular/core';
 })
 export class ProductService {
 
+  baseURL:string = "http://localhost:8000/products/";
+  viewProd : string =  "http://localhost:8000/products/";
+
   constructor(private http: HttpClient) { }
 
-  createProduct(){
-    
+  createProduct(productData: any){
+  return this.http.post(this.baseURL,productData);
+  }
+
+  viewProduct(id : any){
+    return this.http.get(this.viewProd+id);
+  }
+
+  updateroduct(id : any,productData: any){
+    return this.http.put(this.viewProd+id,productData);
+  }
+
+   searchProductByCategory(categoryid : any){
+    let viewProdByCategory =  "http://localhost:8000/products/category="+categoryid;
+    return this.http.get(viewProdByCategory);
+  }
+
+  searchProductByDate(date : any){
+    let viewProdByDate =  "http://localhost:8000/products/date="+date;
+    return this.http.get(viewProdByDate);
   }
 }
